07b39dba0d3454a863ca96aac916fc8978c6ceb4
Renan-HS <105560491+Renan-HS@users.noreply.github.com>
2022-07-06 16:56:11 -0300

    Update Store.js

--- src/client/Store.js
+++ src/client/Store.js
@@ -7,6 +7,7 @@ import { React, useState, useEffect } from 'react'
 import ApiProduto from '../apis/ApiProduto'
 /* import ApiPedido from '../apis/ApiPedido' */
 import { useNavigate, useParams } from 'react-router-dom'
+import Footer from './Footer'
 
 function Store() {
 
@@ -234,6 +235,7 @@ function Store() {
                 </Modal>
 
             </div>
+            <Footer />
         </>
 
     )
