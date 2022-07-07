0b12676f3b82bd497d642da1ffba89f42dc3d415
Renan-HS <105560491+Renan-HS@users.noreply.github.com>
2022-07-07 18:06:51 -0300

    Create Quem.js

---
+++ src/Quem.js
@@ -0,0 +1,33 @@
+import HeaderStore from './client/HeaderStore'
+import React from 'react'
+import Footer from './client/Footer'
+
+
+function Quem() {
+
+
+
+
+    return (
+        <>
+            {
+                
+                        <HeaderStore />
+              
+
+
+            }
+
+
+            <div className="col-sm-6 offset-sm-3">
+                <h2>Orlando</h2>
+                <h2>Renan</h2>
+                <h2>Wellington</h2>
+               
+            </div><br />
+            <Footer />
+        </>
+    )
+}
+
+export default Quem
